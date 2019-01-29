192e722af8e5bd48bf9e4939ea8f4c63303cbe6e
Matthew Titmus <matthew.titmus@gmail.com>
2019-01-28 19:59:18 -0500

    v0.3.2-dev.3 Bundle API stubs

--- service/bundle-handler.go
+++ service/bundle-handler.go
@@ -6,18 +6,6 @@ import (
 	"github.com/gorilla/mux"
 )
 
-// handleDeleteBundleVersion handles "DELETE /v2/bundles/{bundlename}/versions/{version}"
-func handleDeleteBundleVersion(w http.ResponseWriter, r *http.Request) {
-	http.Error(w, "Not Implemented", http.StatusNotImplemented)
-	return
-}
-
-// handleGetBundleVersion handles "GET /v2/bundles/{bundlename}/versions/{version}"
-func handleGetBundleVersion(w http.ResponseWriter, r *http.Request) {
-	http.Error(w, "Not Implemented", http.StatusNotImplemented)
-	return
-}
-
 // handleGetBundles handles "GET /v2/bundles"
 func handleGetBundles(w http.ResponseWriter, r *http.Request) {
 	http.Error(w, "Not Implemented", http.StatusNotImplemented)
@@ -30,16 +18,34 @@ func handleGetBundleVersions(w http.ResponseWriter, r *http.Request) {
 	return
 }
 
-// handlePostBundleVersion handles "POST /v2/bundles/{bundlename}/versions/{version}"
-func handlePostBundleVersion(w http.ResponseWriter, r *http.Request) {
+// handleDeleteBundleVersion handles "DELETE /v2/bundles/{bundlename}/versions/{version}"
+func handleDeleteBundleVersion(w http.ResponseWriter, r *http.Request) {
+	http.Error(w, "Not Implemented", http.StatusNotImplemented)
+	return
+}
+
+// handleGetBundleVersion handles "GET /v2/bundles/{bundlename}/versions/{version}"
+func handleGetBundleVersion(w http.ResponseWriter, r *http.Request) {
+	http.Error(w, "Not Implemented", http.StatusNotImplemented)
+	return
+}
+
+// handlePutBundleVersion handles "PUT /v2/bundles/{bundlename}/versions/{version}"
+func handlePutBundleVersion(w http.ResponseWriter, r *http.Request) {
+	// params := mux.Vars(r)
+	// bundlename := params["bundlename"]
+	// version := params["version"]
+
 	http.Error(w, "Not Implemented", http.StatusNotImplemented)
 	return
 }
 
 func addBundleMethodsToRouter(router *mux.Router) {
 	router.HandleFunc("/v2/bundles", handleGetBundles).Methods("GET")
+
 	router.HandleFunc("/v2/bundles/{bundlename}", handleGetBundleVersions).Methods("GET")
+
 	router.HandleFunc("/v2/bundles/{bundlename}/versions/{version}", handleGetBundleVersion).Methods("GET")
-	router.HandleFunc("/v2/bundles/{bundlename}/versions/{version}", handlePostBundleVersion).Methods("POST")
+	router.HandleFunc("/v2/bundles/{bundlename}/versions/{version}", handlePutBundleVersion).Methods("PUT")
 	router.HandleFunc("/v2/bundles/{bundlename}/versions/{version}", handleDeleteBundleVersion).Methods("DELETE")
 }
