46c7d238f5195b096b29e54b186a5ea5ac83837e
Matthew Titmus <matthew.titmus@gmail.com>
2019-02-18 18:12:42 -0500

    v0.3.6-dev.0 bundle enable and disable support

--- client/client-bundle.go
+++ client/client-bundle.go
@@ -4,15 +4,27 @@ import (
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
+	"net/http"
 
 	"github.com/clockworksoul/cog2/data"
 )
+
+// BundleDisable comments to be written...
+func (c *CogClient) BundleDisable(bundlename string, version string) error {
+	return c.doBundleEnable(bundlename, version, false)
+}
+
+// BundleEnable comments to be written...
+func (c *CogClient) BundleEnable(bundlename string, version string) error {
+	return c.doBundleEnable(bundlename, version, true)
+}
 
 // BundleExists simply returns true if a bundle exists with the specified
 // bundlename; false otherwise.
 func (c *CogClient) BundleExists(bundlename string, version string) (bool, error) {
 	url := fmt.Sprintf("%s/v2/bundles/%s/version/%s",
 		c.profile.URL.String(), bundlename, version)
+
 	resp, err := c.doRequest("GET", url, []byte{})
 	if err != nil {
 		return false, err
@@ -20,9 +32,9 @@ func (c *CogClient) BundleExists(bundlename string, version string) (bool, error
 	defer resp.Body.Close()
 
 	switch resp.StatusCode {
-	case 200:
+	case http.StatusOK:
 		return true, nil
-	case 404:
+	case http.StatusNotFound:
 		return false, nil
 	default:
 		return false, getResponseError(resp)
@@ -32,13 +44,14 @@ func (c *CogClient) BundleExists(bundlename string, version string) (bool, error
 // BundleList comments to be written...
 func (c *CogClient) BundleList() ([]data.Bundle, error) {
 	url := fmt.Sprintf("%s/v2/bundles", c.profile.URL.String())
+
 	resp, err := c.doRequest("GET", url, []byte{})
 	if err != nil {
 		return []data.Bundle{}, err
 	}
 	defer resp.Body.Close()
 
-	if resp.StatusCode != 200 {
+	if resp.StatusCode != http.StatusOK {
 		return []data.Bundle{}, getResponseError(resp)
 	}
 
@@ -72,7 +85,7 @@ func (c *CogClient) BundleInstall(bundle data.Bundle) error {
 	}
 	defer resp.Body.Close()
 
-	if resp.StatusCode != 200 {
+	if resp.StatusCode != http.StatusOK {
 		return getResponseError(resp)
 	}
 
@@ -90,9 +103,30 @@ func (c *CogClient) BundleUninstall(bundlename string, version string) error {
 	}
 	defer resp.Body.Close()
 
-	if resp.StatusCode != 200 {
+	if resp.StatusCode != http.StatusOK {
 		return getResponseError(resp)
 	}
 
 	return nil
+}
+
+// doBundleEnable comments to be written...
+func (c *CogClient) doBundleEnable(bundlename string, version string, enabled bool) error {
+	url := fmt.Sprintf("%s/v2/bundles/%s/versions/%s?enabled=%v",
+		c.profile.URL.String(), bundlename, version, enabled)
+
+	// TODO Get latest if version == 'latest'
+
+	resp, err := c.doRequest("PATCH", url, []byte{})
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	switch resp.StatusCode {
+	case http.StatusOK:
+		return nil
+	default:
+		return getResponseError(resp)
+	}
 }
--- service/bundle-handlers.go
+++ service/bundle-handlers.go
@@ -3,6 +3,9 @@ package service
 import (
 	"encoding/json"
 	"net/http"
+	"strings"
+
+	"github.com/clockworksoul/cog2/dataaccess/errs"
 
 	"github.com/clockworksoul/cog2/data"
 	cogerr "github.com/clockworksoul/cog2/errors"
@@ -68,6 +71,55 @@ func handleGetBundleVersion(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(bundle)
 }
 
+// handlePatchBundleVersion handles "PATCH /v2/bundles/{name}/versions/{version}"
+func handlePatchBundleVersion(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	name := params["name"]
+	version := params["version"]
+
+	exists, err := dataAccessLayer.BundleExists(name, version)
+	if err != nil {
+		respondAndLogError(w, err)
+		return
+	} else if !exists {
+		respondAndLogError(w, errs.ErrNoSuchBundle)
+		return
+	}
+
+	if r.ContentLength > 0 {
+		var bundle data.Bundle
+
+		err = json.NewDecoder(r.Body).Decode(&bundle)
+		if err != nil {
+			respondAndLogError(w, cogerr.ErrUnmarshal)
+			return
+		}
+
+		bundle.Name = name
+		bundle.Version = version
+
+		err = dataAccessLayer.BundleUpdate(bundle)
+		if err != nil {
+			respondAndLogError(w, err)
+			return
+		}
+	}
+
+	enabled := r.FormValue("enabled")
+	if enabled != "" {
+		enabled = strings.ToUpper(enabled)
+		if enabled[0] == 'T' {
+			err = dataAccessLayer.BundleEnable(name, version)
+		} else {
+			err = dataAccessLayer.BundleDisable(name, version)
+		}
+		if err != nil {
+			respondAndLogError(w, err)
+			return
+		}
+	}
+}
+
 // handlePutBundleVersion handles "PUT /v2/bundles/{name}/versions/{version}"
 func handlePutBundleVersion(w http.ResponseWriter, r *http.Request) {
 	var bundle data.Bundle
@@ -99,4 +151,10 @@ func addBundleMethodsToRouter(router *mux.Router) {
 	router.HandleFunc("/v2/bundles/{name}/versions/{version}", handleGetBundleVersion).Methods("GET")
 	router.HandleFunc("/v2/bundles/{name}/versions/{version}", handlePutBundleVersion).Methods("PUT")
 	router.HandleFunc("/v2/bundles/{name}/versions/{version}", handleDeleteBundleVersion).Methods("DELETE")
+
+	router.HandleFunc("/v2/bundles/{name}/versions/{version}", handlePatchBundleVersion).
+		Methods("PATCH").
+		Queries("enabled", "")
+	router.HandleFunc("/v2/bundles/{name}/versions/{version}", handlePatchBundleVersion).
+		Methods("PATCH")
 }
