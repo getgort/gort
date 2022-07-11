7409b28a5a88521b850a66a198adf915ff4ab5ef
Jack Greenberg <jagreenberg@yext.com>
2022-07-11 16:18:23 -0400

    Fix scheduler test

--- scheduler/scheduler_test.go
+++ scheduler/scheduler_test.go
@@ -22,32 +22,45 @@ import (
 	"time"
 
 	"github.com/getgort/gort/command"
+	"github.com/getgort/gort/config"
 	"github.com/getgort/gort/data"
+	"github.com/getgort/gort/data/rest"
+	"github.com/getgort/gort/dataaccess"
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
 func TestCommandSchedulerFull(t *testing.T) {
-	t.SkipNow() // TODO
+	// Init Gort
+	require.NoError(t, config.Initialize("../testing/config/no-database.yml"))
+
+	da, err := dataaccess.Get()
+	require.NoError(t, err)
+
+	require.NoError(t, da.Initialize(context.Background()))
+	user, err := dataaccess.DoBootstrap(context.Background(), rest.User{
+		Email:    "user@getgort.io",
+		Username: "user",
+	})
+	require.NoError(t, err)
 
 	scheduledCommands := StartScheduler()
-
+	entries, err := da.FindCommandEntry(context.Background(), "gort", "whoami")
+	require.NoError(t, err)
+	require.NotEmpty(t, entries)
 	require.NoError(t, Schedule(context.Background(), data.ScheduledCommand{
-		CommandEntry: data.CommandEntry{
-			Bundle:  data.Bundle{},
-			Command: data.BundleCommand{},
-		},
+		CommandEntry: entries[0],
 		Command: command.Command{
-			Bundle:     "",
-			Command:    "",
-			Options:    nil,
-			Parameters: nil,
+			Bundle:     "gort",
+			Command:    "whoami",
+			Options:    make(map[string]command.CommandOption),
+			Parameters: make(command.CommandParameters, 0),
 		},
 		Cron:      "@every 1s",
 		UserID:    "id",
-		UserName:  "name",
-		UserEmail: "email",
+		UserName:  user.Username,
+		UserEmail: user.Email,
 		Adapter:   "test adapter",
 		ChannelID: "channel",
 	}))
